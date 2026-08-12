8957c6e9b19d4ca88eb5d44bc80cf6a77fccb66b
zunaira445 <zunairasahi04@gmail.com>
2026-08-12 15:23:39 +0500

    Fix API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,9 +6,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
